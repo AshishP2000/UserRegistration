fa35e3264dd06379352dab556a6c8ba65b9cc460
Ashish <ashish2000patil@gmail.com>
2022-10-16 05:50:25 +0530

    [Ashish]Added: should atleast contain 1 special character UC-8

--- UserResgistration/RegexPattern.cs
+++ UserResgistration/RegexPattern.cs
@@ -86,15 +86,44 @@ namespace UserResgistration
             string[] arr = {"Ashishasd1@", "Ashisha1", "1256123", "1235"};
             string pattern = @"[A-Z][0-9]{0,1}[a-z]{6,}[0-9]{1,}[!@#$%^&*?-]{1}";
 
+            string ErrorMessage = string.Empty;
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{8,15}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
             foreach (string input in arr)
             {
-                if (Regex.IsMatch(input, pattern))
+                if (!hasLowerChar.IsMatch(input))
                 {
-                    Console.WriteLine("{0} is valid", input);
+                    ErrorMessage = "Password should contain at least one lower case letter.";
+                    Console.WriteLine(ErrorMessage);
+                }
+                else if (!hasUpperChar.IsMatch(input))
+                {
+                    ErrorMessage = "Password should contain at least one upper case letter.";
+                    Console.WriteLine(ErrorMessage);
+                }
+                else if (!hasMiniMaxChars.IsMatch(input))
+                {
+                    ErrorMessage = "Password should not be lesser than 8 or greater than 15 characters.";
+                    Console.WriteLine(ErrorMessage);
+                }
+                else if (!hasNumber.IsMatch(input))
+                {
+                    ErrorMessage = "Password should contain at least one numeric value.";
+                    Console.WriteLine(ErrorMessage);
+                }
+
+                else if (!hasSymbols.IsMatch(input))
+                {
+                    ErrorMessage = "Password should contain at least one special case character.";
+                    Console.WriteLine(ErrorMessage);
                 }
                 else
                 {
-                    Console.WriteLine("{0} is Invalid", input);
+                    Console.WriteLine("{0} Valid", input);
                 }
             }
         }
